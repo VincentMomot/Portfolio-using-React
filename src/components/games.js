3a164ffb743c2e34f1603620b675f4ea4f8aef3b
Vince Momot <vincemomot@gmail.com>
2022-01-30 08:12:48 -0500

    fixed the package.json file

--- src/components/games.js
+++ src/components/games.js
@@ -2,7 +2,46 @@ import React from 'react';
 
 function home() {
   return (<div>
-       
+
+      <main id="background">
+        <h1>Scratch Games</h1>
+        <hr />
+        <h2>Defeat the Mice</h2>
+        <iframe src="https://scratch.mit.edu/projects/559282374/embed" allowTransparency="true" frameBorder={0} scrolling="no" allowFullScreen="yes" />
+        <p><strong><u>Instructions</u></strong><br />
+          Arrow keys move the cat left and right<br />
+          Space bar shoots a ball at the falling mice<br />
+          Avoid touching falling poison apples <br />
+          Keep your ammunition in check<br />
+          Don't let the mice get to the purple line!<br />
+          For a better full screen experience, visit:<br />
+          <a href="https://scratch.mit.edu/projects/559282374" target="_blank">Scratch Website</a><br /><br />
+          Difficulty: 5/10 <br />
+          Send me your high score in the Contact Me section<br />
+          in the home page!<br />
+          Current Record: 218 mice <br />
+          Held By: Vincent D Momot
+          <br /><br /><br /><br />
+        </p>  
+        <br />
+        <h2>Defend the Princess</h2>
+        <hr />
+        <iframe src="https://scratch.mit.edu/projects/561662201/embed" allowTransparency="true" frameBorder={0} scrolling="no" allowFullScreen />
+        <p><strong><u>Instructions</u></strong><br /><br />
+          Arrow keys move the dragon in all directions<br />
+          Hold the space bar breathe fire<br />
+          Don't let the soldiers reach the tower<br />
+          If you kill the griffin you get a power up<br />
+          Don't let your fire meter depleat! <br />
+          For a better full screen experience, visit:<br />
+          <a href="https://scratch.mit.edu/projects/561662201" target="_blank">Scratch Website</a><br /><br />
+          Difficulty: 2/10 <br />
+          Send me your high score in the Contact Me section<br />
+          in the home page!<br />
+          Current Record: 1000 sec <br />
+          Held By: Vincent D Momot
+        </p>
+      </main>  
         
   </div>);
 }
--- src/components/myWork.js
+++ src/components/myWork.js
@@ -2,7 +2,72 @@ import React from 'react';
 
 function home() {
   return (<div>
-       
+        <h2>JustFurPets</h2>
+        <hr />
+        <a href="https://just-fur-pets.herokuapp.com/" target="_blank">
+          <img src="Assets/PetSS.png" alt="Screenshot of Pet Management website" />
+        </a>
+        <p>This site was a group project designed for a pet daycare manager to keep track of all the pets within the facility</p>
+        <a className="git" href="https://github.com/VincentMomot/justFurPets" target="_blank">Click here to visit the github page</a>
+        <br />
+        <h2>My First Website</h2>
+        <hr />
+        <a href="Assets/fanpage.html" target="_blank">
+          <img src="Assets/FOB.jpeg" alt="My First Website" />
+        </a>
+        <p>This is my first website that I created in August 2021. <b id="black">Click on the image to visit it.</b></p>
+        <br />
+        <h2>API Weather Application</h2>
+        <hr />
+        <a href="https://vincentmomot.github.io/Weather-Application-by-Vince/" target="_blank">
+          <img src="Assets/weatherShot.png" alt="My Weather Site" />
+        </a>
+        <p>This is my first utilization of APIs to grab weather data based on the user's city chosen.<b id="black">Click on the image to visit it.</b></p>
+        <a className="git" href="https://github.com/VincentMomot/Weather-Application-by-Vince" target="_blank">Click here to visit the github page</a>
+        <br />
+        <h2>Day Planner</h2>
+        <hr />
+        <a href="https://vincentmomot.github.io/Planner-Application-by-Vince" target="_blank">
+          <img src="Assets/plannerShot.png" alt="My Planner Site" />
+        </a>
+        <p>In this application I used DOM manipulation, time methods, and local storage functionality in order to provide the user with a way to organize their day.</p>
+        <br />
+        <p><b id="black">Click on the image to visit it.</b></p>
+        <a className="git" href="https://github.com/VincentMomot/Planner-Application-by-Vince" target="_blank">Click here to visit the github page</a>
+        <br />
+        <h2>Password Generator with JS</h2>
+        <hr /><br />
+        <a href="https://vincentmomot.github.io/Create-Your-Password-with-JS/" target="_blank">
+          <img id="pswd" src="Assets/pswd.png" alt="Password Generator Website" />
+        </a>
+        <p>This is my first JS code that will generate a password for you! <b id="black">Click on the image to visit it.</b></p>
+        <a className="git" href="https://github.com/VincentMomot/Create-Your-Password-with-JS" target="_blank">Click here to visit the github page</a>
+        <br />
+        <h2>UAS Constructions</h2>
+        <hr />
+        <img src="Assets/CNCBody.jpg" alt="CNC machining the side of the body of the UAS" />
+        <img src="Assets/CNCWing.jpg" alt="CNC machining the wing" />
+        <img src="Assets/WindTunnel.jpg" alt="Wind Tunnel Testing the Agricultural UAS" />
+        <p>The three blue foam images are for a project to develop a drone to fly over crop fields to evaluate crop health.</p>
+        <p>This involved using a windtunnel to gather experimental data to back up our claims on the performance of the wing.</p><br />
+        <img src="Assets/JARVIFTeam.jpg" alt="JARVIF Team working on the UAS" />
+        <img src="Assets/plane.jpg" alt="Partially Assembled UAS" />
+        <p>A team that I lead was called the JARVIF team. We designed and constructed a UAS in only two month's time</p>
+        <p>This was a reaserach project to check the accuracuy of AAA software which is used in commercial aircraft evaluation. </p>
+        <br />
+        <img src="Assets/LTA1.jpg" alt="Structural insides of the UAS" /> 
+        <img src="Assets/Group10.jpg" alt="LTA group picture holding the UAS" />
+        <p>This competition team won first place. We were given a baloon to carry 85% of the aircrat's weight and then fly through an obstacle cource.</p>
+        <p>I was in charge of piloting the UAS, programming the controller, and some construction. </p>
+        <h2>Solidworks Model</h2>
+        <hr />
+        <img src="Assets/IsometricBottle.JPG" alt="Isometric View of a Watterbottle Created in SolidWorks" />
+        <img src="Assets/Bottle.JPG" alt="Side View of a Watterbottle Created in SolidWorks" />
+        <img src="Assets/Top.JPG" alt="The top piece of a Watterbottle Created in SolidWorks" />
+        <img src="Assets/SectionBottle.JPG" alt="The Section View of a Watterbottle Created in SolidWorks" />
+        <p>These images showcase my SolidWorks skills using only a 12" ruler and a waterbottle to take measurements on.</p>
+        <p>With this project, I learned how to apply screw constraints and render images to look more impressive.</p>
+ 
         
   </div>);
 }
